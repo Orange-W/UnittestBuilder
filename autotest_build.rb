d729743a7a60f5f7a2c3018144fc56acf3135755
Huey <hueywang@tencent.com>
2020-04-07 17:38:03 +0800

    add method

--- autotest_build.rb
+++ autotest_build.rb
@@ -5,16 +5,90 @@ require 'fileutils'
 require_relative 'trollop'
 require 'json'
 require 'pathname'
+require 'xcodeproj'
 
+## 查询正则项第一个所在行
+def findGrepStringLine(filePath, grepString) 
+    result = `cat #{filePath} | grep -n -e "#{grepString}"`
+    return result.split(":").first.to_i
+end
 
-def testFileInit(file, fileName, newFileName, testdir)
+# 替换文件中的关键字
+def replaceContent(file, afterContent, replaceContent) 
+    findImplementation = findGrepStringLine(file, afterContent)
+    if findImplementation == 0 then return; end
 
+    oldfileHandle = File.new(file, 'r')
+    tmpFile = file + ".tmp"
+    oldfileReplaceHandle = File.new(tmpFile, 'w')
+    puts findImplementation
+    lineNum = 0
+    oldfileHandle.each do |line|
+        lineNum += 1
+        oldfileReplaceHandle.puts line
+        if lineNum == findImplementation
+            puts "行数:#{lineNum}, #{line}"
+            oldfileReplaceHandle.puts replaceContent
+        end
+    end
+    oldfileHandle.close
+    oldfileReplaceHandle.close
+    `mv #{tmpFile} #{file}`
+end
+
+def testFileInit(newFile, oldfile, newFileName, testdir)
+    newFileHandle = File.new(newFile, "w+")
+    oldFileName = oldfile.split("/").last
     gitname = `git config user.name`.strip
     date = Time.new.strftime("%Y-%m-%d").strip
     className = newFileName.gsub(".m", "")
-    testClassName = fileName.gsub(".m", "")
+    testClassName = oldFileName.gsub(".m", "")
+
+    #--- 检查是否有init函数
+    findInitFunc = findGrepStringLine(oldfile, "-[ ]*(instancetype)[ ]*init[ ]*{")
+    findImplementation = findGrepStringLine(oldfile, "@implementation[ ]*#{testClassName}")
+
+    if findInitFunc == 0 && findImplementation > 0
+        # 如果没有找到, 插入init函数
+        # File.open(oldfile, 'w') {|file| file.puts replace}
+        replaceContent = <<-EOF
+\n//-- autobuild init --//
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
+//-- autobuild init end --//
+                EOF
+
+        replaceContent(oldfile, "@implementation[ ]*#{testClassName}", replaceContent)
+#         oldfileHandle = File.new(oldfile, 'r+')
+#         tmpFile = oldfile + ".tmp"
+#         oldfileReplaceHandle = File.new(tmpFile, 'w')
+#         lineNum = 0
+#         oldfileHandle.each do |line|
+#             lineNum += 1
+#             oldfileReplaceHandle.puts line
+#             if lineNum == findImplementation
+#                 puts "行数:#{lineNum}, #{line}"
+
+#                 oldfileReplaceHandle.puts <<-EOF
+# \n//-- autobuild init --//
+# - (instancetype)init {
+#     self = [super init];
+#     return self;
+# }
+# //-- autobuild init end --//
+#                 EOF
+#             end
+            
+#         end
+#         oldfileHandle.close
+#         oldfileReplaceHandle.close
+#         `mv #{tmpFile} #{oldfile}`
+    end
+
     # ------
-    file.puts <<-EOF
+    newFileHandle.puts <<-EOF
 //
 //  #{newFileName}
 //  live4iphoneTests
@@ -62,9 +136,42 @@ def testFileInit(file, fileName, newFileName, testdir)
 //--- end ---//
 @end
     EOF
+    newFileHandle.close
+end
+
+def addFilesToGroup(project_path)
+    # project = Xcodeproj::Project.open(project_path)
+    # target = project.targets.first
+    # filePath = "./qqlive4iphone/VBHttpNetworkRequestConfig-Imp/QLHttpRequestNacHelper.m"
+
+    # group_path = File.join("CommonProject", "SVGKit", "usr", "asc", "xx")
+    # puts "group_path:#{group_path}"
+    # fatherGroup = project.main_group.find_subpath(group_path, true)
+    # fatherGroup.set_source_tree('<group>')
+    # fatherGroup.set_path("xxx2")
+
+    # puts "fatherGroup:#{fatherGroup}"
+    # 过滤目录和.DS_Store文件
+    # if !File.directory?(filePath) && entry != ".DS_Store" then
+        # 特殊逻辑
+
+        # 向group中增加文件引用
+        # fileReference = fatherGroup.new_reference(filePath)
+        # # 如果不是头文件则继续增加到Build Phase中，PB文件需要加编译标志
+        # if filePath.to_s.end_with?("pbobjc.m", "pbobjc.mm") then
+        #     target.add_file_references([fileReference], '-fno-objc-arc')
+           
+        # elsif filePath.to_s.end_with?(".m", ".mm", ".cpp") then
+        #     target.source_build_phase.add_file_reference(fileReference, true)
+        #     puts "m"
+        # elsif filePath.to_s.end_with?(".plist") then
+        #     target.resources_build_phase.add_file_reference(fileReference, true)
+        # end
+        # exit;
 end
 
 if __FILE__ == $0
+    # addFilesToGroup("./qqlive4iphone/live4iphone.xcodeproj")
     opts = Trollop::options do
         opt :dir, 'Path for xcresult file', :type => :string
         opt :aim_dir, 'Path for xcresult file', :type => :string
@@ -95,8 +202,14 @@ if __FILE__ == $0
         fileHasCreate = FileTest::exists?(newFile)
         if fileHasCreate
             # 存在就跳过
-            puts "跳过#{newFile}"
+            puts "跳过已存在文件#{newFile}"
             exit;
+            next;
+        end
+
+        if newFile.include?("+")
+            # 文件包含+号，是扩展
+            puts "跳过扩展#{newFile}"
             next;
         end
 
@@ -106,13 +219,10 @@ if __FILE__ == $0
         end
         
         puts "newFile:#{newFile}"
-        newFileHandle = File.new(newFile, "w+")
-
-        testdir = filepath[filepath.index(dir)...]
-        testFileInit(newFileHandle, fileName, newFileName, testdir)
         
 
-        newFileHandle.close
+        testdir = filepath[filepath.index(dir)...]
+        testFileInit(newFile, file, newFileName, testdir)
         exit;
         # 不存在创建文件
 
